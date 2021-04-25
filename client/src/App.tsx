b3b2e3ad52270561df398404d8b28908d934ff64
greatmaxix <greatmaxix@gmail.com>
2021-04-25 18:57:22 +0600

    product page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,29 @@ import { Route } from 'react-router';
 import './App.css';
 import ProductsPage from './pages/products/ProductsPage';
 import SignInPage from './pages/signin/SignInPage';
+import { Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+import SignUpPage from './pages/signup/SignUpPage';
 
 function App() {
   return (
     <Fragment>
-        <Route exact path="/" component={SignInPage} />
-        <Route path="/signin/" component={SignInPage} />
-        <Route path="/products/" component={ProductsPage}/>
-        {/* <Route path="/signup/" component={SignUpPage} /> */}
-        {/* <Route exact path="/tasks" component={TasksPage} />
+      <Nav>
+        <Nav.Item>
+          <Link className="nav-link" to="/products">Products</Link>
+        </Nav.Item>
+      </Nav>
+      <div className="container-fluid">
+        <Route path="/" component={ProductsPage} />
+        <Route exact path="/signin" component={SignInPage} />
+        <Route path="/signup/" component={SignUpPage} />
+        <Route path="/products" component={ProductsPage} />
+      </div>
+
+      {/* <Route path="/signup/" component={SignUpPage} /> */}
+      {/* <Route exact path="/tasks" component={TasksPage} />
         <Route exact path="/tasks/create" component={CreateTaskPage} /> */}
-      </Fragment>
+    </Fragment>
   );
 }
 

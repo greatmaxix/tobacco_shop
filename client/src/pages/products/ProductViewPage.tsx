a304812caee466f2470a6ff36dc2a95178d3cabd
greatmaxix <greatmaxix@gmail.com>
2021-04-26 23:00:59 +0600

    product view page

--- client/src/pages/products/ProductViewPage.tsx
+++ client/src/pages/products/ProductViewPage.tsx
@@ -51,53 +51,59 @@ export default class ProductsViewPage extends React.Component<ProductsViewPagePr
 
     async reload() {
         console.log(this.props);
+        let product = null;
         let result = await this.state.service.getProduct(this.props.match.params.id);
         if (result && result.data) {
+            let imagesResult = await this.state.imageService.fetchImages({
+                "imageable_type": "products",
+                "imageable_id": result.data.id
+            });
+            product = result.data;
+            product.images = imagesResult && imagesResult.data && imagesResult.data.length > 0 ? imagesResult.data : [];
             this.setState({
-                product: result.data
-            }
-            )
+                product: product
+            })
         }
         console.log(this.state.product)
         let productType = await this.state.service.getProductType(this.props.match.params.id);
         console.log(productType)
     }   
-    // getProductsCarousel(product: ProductType | null) {
-    //     return (
-    //         <Carousel>
-    //             {product && product.images.length > 0  ? product.images.map((image: ImageType, index) => {
-    //                 let src = image && image.mimetype && image.image_blob && image.image_blob.data ? `data:${image.mimetype};base64,${Buffer.from(image.image_blob.data).toString('base64')}` : '';
-    //                 return <Carousel.Item>
-    //                     <img
-    //                         className="d-block img-thumbnail"
-    //                         src={src}
-    //                         alt={image.alt_text}
-    //                     />
-    //                     <Carousel.Caption>
-    //                         <h3>{product.cost + " тг"}</h3>
-    //                     </Carousel.Caption>
-    //                 </Carousel.Item>
-    //             }) : <Carousel.Item>
-    //                 <img
-    //                     className="d-block img-thumbnail"
-    //                     src={blankProduct}
-    //                     alt={product?.title}
+    getProductsCarousel(product: ProductType | null) {
+        return (
+            <Carousel>
+                {product && product.images.length > 0  ? product.images.map((image: ImageType, index) => {
+                    let src = image && image.mimetype && image.image_blob && image.image_blob.data ? `data:${image.mimetype};base64,${Buffer.from(image.image_blob.data).toString('base64')}` : '';
+                    return <Carousel.Item>
+                        <img
+                            className="d-block img-thumbnail"
+                            src={src}
+                            alt={image.alt_text}
+                        />
+                        <Carousel.Caption>
+                            <h3>{product.cost + " тг"}</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                }) : <Carousel.Item>
+                    <img
+                        className="d-block img-thumbnail"
+                        src={blankProduct}
+                        alt={product?.title}
 
-    //                 />
-    //                 <Carousel.Caption>
-    //                     <h3>{product?.cost + " тг"}</h3>
-    //                 </Carousel.Caption>
-    //             </Carousel.Item>}
-    //         </Carousel>
-    //     )
-    // }
+                    />
+                    <Carousel.Caption>
+                        <h3>{product?.cost + " тг"}</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>}
+            </Carousel>
+        )
+    }
 
     render() {
         return (
-            <div className="container-fluid">
+            <div className="container-fluid text-center">
             <Card>
             <Card.Body>
-                {/* {this.getProductsCarousel(this.state.product)} */}
+                {this.getProductsCarousel(this.state.product)}
             </Card.Body>
             <Card.Footer className="text-center">
                 <Card.Title>{this.state.product?.title}</Card.Title>
